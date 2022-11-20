b6a2412d68f2213b38947a8600257ac91a79a733
Deivis Cervantes <dcervantes@utb.edu.co>
2022-11-20 13:14:56 -0500

    Se crearon dos rutas para saber cómo mostrar vistas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('bienvenida');
+});
+
+Route::get('/sobre-nosotros', function () {
+    return view('sobre-nosotros');
+});
+
+Route::get('/contacto', function () {
+    return view('contacto');
 });
