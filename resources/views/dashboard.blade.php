16a2b7df5def4b431103143360d31e025c039fc1
Deivis Cervantes <dcervantes@utb.edu.co>
2022-11-20 15:30:25 -0500

    Accedimos a la información del usuario logueado a través de la función user() de la fachada Auth

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    You're logged in!
+                    <p>Hola {{ Auth::user()->name }}, estás logueado!</p>
+                    <p>Tu correo electrónico es {{ Auth::user()->email }}.</p>
                 </div>
             </div>
         </div>
